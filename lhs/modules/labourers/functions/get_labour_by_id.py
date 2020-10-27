4918e131c4f978ac1c87c9b0da416ee073c6ed78
ajayp <0xajaykumar@gmail.com>
2020-10-27 14:12:11 +0530

    labour get data

--- lhs/modules/labourers/functions/get_labour_by_id.py
+++ lhs/modules/labourers/functions/get_labour_by_id.py
@@ -16,7 +16,9 @@ def get_access_token(refresh_token):
         }
         header = {"Authorization": "Basic "+authorization_code.decode('ascii')}
         token = requests.post("https://api.fitbit.com/oauth2/token", data=payload, headers=header).json()
-        return token["access_token"]
+        if 'errors' in token:
+            raise Exception(token['errors'][0]['errorType'])
+        return token
     except Exception as err:
         raise Exception(err)
 
@@ -24,9 +26,27 @@ def get_labour_by_id(labour_id, user_data):
     try:
         labour = Labour.objects(pk=labour_id,visibility=True).first()
         result = labour.to_dict()
-        access_token = get_access_token(result["temp_credentials"]["refresh_token"])
-        header = {"Authorization": "Bearer "+access_token}
+        token = get_access_token(result["temp_credentials"]["refresh_token"])
+        labour.temp_credentials = token
+        header = {"Authorization": "Bearer "+token["access_token"]}
         activity = requests.get('https://api.fitbit.com/1/user/'+result["temp_credentials"]["user_id"]+'/activities/date/'+str(datetime.now().date())+'.json', headers=header).json()
-        return {"statusCode":200, "message":"get labour data successful","data":activity}
+        heart_rate = requests.get('https://api.fitbit.com/1/user/'+result["temp_credentials"]["user_id"]+'/activities/heart/date/'+str(datetime.now().date())+'/1d.json', headers=header).json()
+        labour.save()
+        return {"statusCode":200, "message":"get labour data successful","data":{'activity':activity,'heart_rate':heart_rate}},200
+    except Exception as err:
+        return {"statusCode":500, "message":str(err)},500
+
+
+def get_labour_range_by_id(labour_id,from_date,end_date,user_data):
+    try:
+        labour = Labour.objects(pk=labour_id,visibility=True).first()
+        result = labour.to_dict()
+        token = get_access_token(result["temp_credentials"]["refresh_token"])
+        labour.temp_credentials = token
+        header = {"Authorization": "Bearer "+token["access_token"]}
+        activity = requests.get('https://api.fitbit.com/1/user/'+result["temp_credentials"]["user_id"]+'/activities/date/'+from_date+'/'+end_date+'.json', headers=header).json()
+        heart_rate = requests.get('https://api.fitbit.com/1/user/'+result["temp_credentials"]["user_id"]+'/activities/heart/date/'+from_date+'/'+end_date+'.json', headers=header).json()
+        labour.save()
+        return {"statusCode":200, "message":"get labour data successful","data":{'activity':activity,'heart_rate':heart_rate}},200
     except Exception as err:
         return {"statusCode":500, "message":str(err)},500
